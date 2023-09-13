a8e71a9b2f2666481aeb4676a96180111a6c0a2c
HowDoIprintHelloWorld <87938212+HowDoIprintHelloWorld@users.noreply.github.com>
2023-09-13 14:37:43 +0000

    Added start of json reading

--- src/tools/config.rs
+++ src/tools/config.rs
@@ -1,16 +1,23 @@
 use std::collections::HashMap;
-use std::{fs, env};
+use std::{fs, env, process, str};
+use serde_json;
 
 
 fn getConfigFileContents() -> String {
     let mut configFileContents = String::new();
     let args = env::args().collect::<Vec<String>>();
-    let fileLocation = match args.get(0) {
+    let fileLocation = match args.get(1) {
         Some(arg) => {arg.to_string()},
         None => {"".to_string()},
     };
-    fs::read("");
-
+    println!("{}", fileLocation);
+    let file = fs::read_to_string(fileLocation);
+    let fileContentUtf8 = match file {
+      Ok(fileContent) => {fileContent},
+      Err(e) => {panic!("{}", format!("Error reading config file: {:?}", e))}
+    };
+    let fileContent = serde_json::from_str(&fileContentUtf8[..]);
+    println!("Content {:?}", fileContent);
     return configFileContents;
 }
 
